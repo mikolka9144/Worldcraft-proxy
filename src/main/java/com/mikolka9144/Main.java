a64e9aa6cadd26fe5072267e28f76d82cce86b0c
Mikołaj Chajewski <51335952+mikolka9144@users.noreply.github.com>
2022-11-04 19:14:49 +0100

    Additions

--- src/main/java/com/mikolka9144/Main.java
+++ src/main/java/com/mikolka9144/Main.java
@@ -1,8 +1,10 @@
 package com.mikolka9144;
 
 import com.mikolka9144.Impl.ChatCommandsInterceptor;
-import com.mikolka9144.Models.Interceptors.ServerInterceptorFunc;
+import com.mikolka9144.Impl.DeviceSpoofer;
+import com.mikolka9144.Impl.PurchaseFaker;
 import com.mikolka9144.Models.Interceptors.ClientInterceptorFunc;
+import com.mikolka9144.Models.Interceptors.ServerInterceptorFunc;
 import com.mikolka9144.Worldcraft.ServerComponents.WorldcraftServer;
 
 import java.io.IOException;
@@ -11,12 +13,13 @@ import java.util.List;
 public class Main {
     public static void main(String[] args) {
         ClientInterceptorFunc reqInterceptors  = (client, server) -> List.of(
-            new ChatCommandsInterceptor(client,server)
+            new ChatCommandsInterceptor(client,server),
+                new DeviceSpoofer(client)
         );
         ServerInterceptorFunc respInterceptors = client -> List.of(
-
+            new PurchaseFaker(client)
         );
-        try(WorldcraftServer server = WorldcraftServer.configureLegacy(reqInterceptors,respInterceptors)){
+        try(WorldcraftServer server = WorldcraftServer.configureWorldcraftDefault(reqInterceptors,respInterceptors)){
             //new Thread(() -> {
              //   try {
              //       WorldcraftServer.configureLegacy(reqInterceptors,respInterceptors).start();
---
+++ src/main/java/com/mikolka9144/Models/EventCodecs/LoginInfo.java
@@ -0,0 +1,88 @@
+package com.mikolka9144.Models.EventCodecs;
+
+public class LoginInfo {
+    private String username;
+    private short skinId;
+    private String clientVer;
+
+    public String getMarketName() {
+        return marketName;
+    }
+
+    public void setMarketName(String marketName) {
+        this.marketName = marketName;
+    }
+
+    private String marketName;
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public short getSkinId() {
+        return skinId;
+    }
+
+    public void setSkinId(short skinId) {
+        this.skinId = skinId;
+    }
+
+    public String getClientVer() {
+        return clientVer;
+    }
+
+    public void setClientVer(String clientVer) {
+        this.clientVer = clientVer;
+    }
+
+    public String getDeviceId() {
+        return deviceId;
+    }
+
+    public void setDeviceId(String deviceId) {
+        this.deviceId = deviceId;
+    }
+
+    public String getDeviceName() {
+        return deviceName;
+    }
+
+    public void setDeviceName(String deviceName) {
+        this.deviceName = deviceName;
+    }
+
+    public String getAndroidVer() {
+        return androidVer;
+    }
+
+    public void setAndroidVer(String androidVer) {
+        this.androidVer = androidVer;
+    }
+
+    public String getAndroidAPI() {
+        return androidAPI;
+    }
+
+    public void setAndroidAPI(String androidAPI) {
+        this.androidAPI = androidAPI;
+    }
+
+    public LoginInfo(String username, short skinId, String clientVer, String deviceId, String deviceName, String androidVer, String androidAPI,String marketName) {
+        this.username = username;
+        this.skinId = skinId;
+        this.clientVer = clientVer;
+        this.deviceId = deviceId;
+        this.deviceName = deviceName;
+        this.androidVer = androidVer;
+        this.androidAPI = androidAPI;
+        this.marketName = marketName;
+    }
+
+    private String deviceId;
+    private String deviceName;
+    private String androidVer;
+    private String androidAPI;
+}
