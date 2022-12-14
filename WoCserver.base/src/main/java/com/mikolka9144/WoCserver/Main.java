aa5ab74f9eb585861a773a3992f89d480ed7d215
Mikołaj Chajewski <51335952+mikolka9144@users.noreply.github.com>
2022-12-14 13:33:40 +0100

    save

--- WoCserver.base/src/main/java/com/mikolka9144/WoCserver/Main.java
+++ WoCserver.base/src/main/java/com/mikolka9144/WoCserver/Main.java
@@ -11,6 +11,7 @@ public class Main {
             String hostname = args[1]; //64.237.54.60
             String config = args[0];
             int port = Integer.parseInt(args[2]); //443
+            int hostingPort = Integer.parseInt(args[3]); //443
             switch (config){
                 case "default":
                     DefaultWoC.main(hostname,port);
@@ -29,7 +30,7 @@ public class Main {
 
             System.out.println("You need to specify configuration for a server");
             System.out.println("Avaliable configurations: default legacy cmd");
-            System.out.println("Argument layout ./server <config> <target-hostname> <target-ip>");
+            System.out.println("Argument layout ./server <config> <target-hostname> <target-port> <host-port>");
         }
 
 
