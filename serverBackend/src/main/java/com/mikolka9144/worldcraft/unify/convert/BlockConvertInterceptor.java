b785e6f3ab45201ff9dbf580285399c5031113e1
Mikołaj Chajewski <mikolajchajewski@gmail.com>
2023-12-13 21:32:40 +0100

    Fixed modules. Now rewirite exporting scripts

---
+++ serverBackend/src/main/java/com/mikolka9144/worldcraft/unify/convert/BlockConvertInterceptor.java
@@ -0,0 +1,44 @@
+package com.mikolka9144.worldcraft.unify.convert;
+
+import com.mikolka9144.packet.packet.Packet;
+import com.mikolka9144.packet.packet.codecs.Block;
+import com.mikolka9144.packet.packet.codecs.LoginInfo;
+import com.mikolka9144.packet.packet.codecs.RoomsPacket;
+import com.mikolka9144.packet.packet.codecs.ServerBlockData;
+import com.mikolka9144.packet.packet.encodings.PacketDataEncoder;
+import com.mikolka9144.worldcraft.unify.backend.VersionFlags;
+import com.mikolka9144.worldcraft.socket.api.PacketsFormula;
+import com.mikolka9144.worldcraft.socket.interceptor.CommandPacketInterceptor;
+import org.springframework.beans.factory.config.ConfigurableBeanFactory;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+@Component("block-convert")
+@Scope(ConfigurableBeanFactory.SCOPE_PROTOTYPE)
+public class BlockConvertInterceptor extends CommandPacketInterceptor {
+    private VersionFlags flags;
+
+    @Override
+    public void interceptLogin(Packet packet, LoginInfo data, PacketsFormula formula) {
+        flags = VersionFlags.getFlags(packet.getProtoId());
+    }
+
+    @Override
+    public void interceptRoomsResp(Packet packet, RoomsPacket data, PacketsFormula formula) {
+        if (flags.simplifyBlocks()){
+           data.getRooms().forEach(s -> s.setId(-s.getId()));
+            packet.setData(PacketDataEncoder.roomsQueryResponse(data));
+        }
+    }
+    @Override
+    public void interceptServerBlocks(Packet packet, ServerBlockData data, PacketsFormula formula) {
+        if (!flags.simplifyBlocks()) return;
+        data.getBlocks().forEach(s -> s.setBlockType(PacketConverter.replaceForWorldcraft(s.getBlockType())));
+        packet.setData(PacketDataEncoder.serverBlocks(data));
+    }
+
+    @Override
+    public void interceptServerPlaceBlock(Packet packet, Block data, PacketsFormula formula) {
+        data.setBlockType(PacketConverter.replaceForWorldcraft(data.getBlockType()));
+        packet.setData(PacketDataEncoder.serverPlaceBlock(data));
+    }
+}
